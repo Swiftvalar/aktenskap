2b398af23a6c5b041db5ceb1d8b155cd413c1fe7
Swiftvalar <44329379+Swiftvalar@users.noreply.github.com>
2019-03-30 19:51:33 -0500

    Refomated to button links

--- src/app/tools.service.ts
+++ src/app/tools.service.ts
@@ -9,7 +9,7 @@ export class ToolsService {
 
   getTools() {
     //return a static array for now. In the future this will come from some kind of endpoint or storage file
-    return [{name: 'Task List', path: 'tasklist'},
+    return [{name: 'Tasklist', path: 'tasklist'},
      {name: 'Future Tool', path: 'futuretool'}];
   };
 }
