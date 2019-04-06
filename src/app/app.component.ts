528fc2e4a197526daafdb636dd61a30236315a65
Swiftvalar <44329379+Swiftvalar@users.noreply.github.com>
2019-04-06 16:08:45 -0500

    Using a navbar instead of buttons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Aktenskap';
+  title = 'Aktenskap!';
+  slogan = 'Tools for Family';
 }
