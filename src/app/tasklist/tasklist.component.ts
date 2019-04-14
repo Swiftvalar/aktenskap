9de5294ab1daa2dbba1e4a3347083a99df1d82a5
Swiftvalar <44329379+Swiftvalar@users.noreply.github.com>
2019-04-13 22:33:58 -0500

    Added new card functionality
    Work in Progress

--- src/app/tasklist/tasklist.component.ts
+++ src/app/tasklist/tasklist.component.ts
@@ -8,10 +8,81 @@ import * as $ from 'jquery/dist/jquery.min.js';
 })
 export class TasklistComponent implements OnInit {
 
+  currentNumberOfRows:number = 2;
+
   constructor() { 
   }
 
   ngOnInit() {
+  }
+
+  newCard() {
+    let lastRowElement = document.getElementById('row_' + this.currentNumberOfRows);
+    let newNumberOfRows:number = this.currentNumberOfRows + 1;
+
+    let row = document.createElement('div');
+    row.id = "row_" + newNumberOfRows;
+    row.className = "row justify-content-md-center";
+
+    let todoColumn = document.createElement('div');
+    todoColumn.id = "todoColumn_" + newNumberOfRows;
+    todoColumn.className = "col";
+
+    let inprogressColumn = document.createElement('div');
+    inprogressColumn.id = "inprogressColumn_" + newNumberOfRows;
+    inprogressColumn.className = "col";
+
+    let reviewColumn = document.createElement('div');
+    reviewColumn.id = "reviewColumn_" + newNumberOfRows;
+    reviewColumn.className = "col";
+
+    let completeColumn = document.createElement('div');
+    completeColumn.id = "todoColumn_" + newNumberOfRows;
+    completeColumn.className = "col";
+
+    let card = document.createElement('div');
+    card.id = "card_" + newNumberOfRows;
+    card.className = "card";
+    
+    let h5 = document.createElement('h5');
+    h5.className = "card-header primary-color white-text";
+    h5.textContent = "Card " + newNumberOfRows;
+
+    let cardBody = document.createElement('div');
+    cardBody.id = "cardBody_" + newNumberOfRows;
+    cardBody.className = "card-body";
+
+    let cardText = document.createElement('div');
+    cardText.className = "card-text";
+    cardText.textContent = "I'm card " + newNumberOfRows;
+
+    let leftButton = document.createElement('button');
+    leftButton.textContent = "Move Left";
+    leftButton.addEventListener ("click", function() {
+        alert("did left something");
+    });
+
+    let rightButton = document.createElement('button');
+    rightButton.textContent = "Move Right";
+    let newCardId:string = card.id;
+    rightButton.addEventListener ("click", function() {
+      alert("did right something");
+    });
+
+    $(row).insertAfter(lastRowElement);
+
+    document.getElementById(row.id).appendChild(todoColumn);
+    document.getElementById(todoColumn.id).appendChild(card);
+    document.getElementById(card.id).appendChild(h5);
+    document.getElementById(card.id).appendChild(cardBody);
+    document.getElementById(cardBody.id).appendChild(cardText);
+    document.getElementById(cardBody.id).appendChild(leftButton);
+    document.getElementById(cardBody.id).appendChild(rightButton);
+    document.getElementById(row.id).appendChild(inprogressColumn);
+    document.getElementById(row.id).appendChild(reviewColumn);
+    document.getElementById(row.id).appendChild(completeColumn);
+
+    this.currentNumberOfRows = newNumberOfRows;
   }
 
   moveRight(cardId) {
@@ -48,6 +119,7 @@ export class TasklistComponent implements OnInit {
         document.getElementById(cardId)
       );
     }
+
   }
 
   moveLeft(cardId) {
@@ -84,5 +156,6 @@ export class TasklistComponent implements OnInit {
         document.getElementById(cardId)
       );
     }
+
   }
 }
