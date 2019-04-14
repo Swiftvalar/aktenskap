d28ffb6dab6f5775cd9b295b4f3b32fae900cf58
Swiftvalar <44329379+Swiftvalar@users.noreply.github.com>
2019-04-13 20:25:48 -0500

    Right and Left buttons

--- src/app/tasklist/tasklist.component.ts
+++ src/app/tasklist/tasklist.component.ts
@@ -14,4 +14,75 @@ export class TasklistComponent implements OnInit {
   ngOnInit() {
   }
 
+  moveRight(cardId) {
+
+    let currentColId:string = document.getElementById(cardId).parentElement.id;
+    let columnArray:any = currentColId.split("_");
+    let columnName:string = columnArray[0];
+    let rowNumber:string = columnArray[1];
+    console.log("Column Name: " + columnName);
+    console.log("Row Number: " + rowNumber);
+
+    let rightColumn:string = '';
+    switch(columnName) {
+      case "todoColumn": {
+        rightColumn = "inprogressColumn";
+        break;
+      }
+      case "inprogressColumn": {
+        rightColumn = "reviewColumn";
+        break;
+      }
+      case "reviewColumn": {
+        rightColumn = "completeColumn";
+        break;
+      }
+      default: {
+        console.log("Invalid: No more columns to the right!")
+        break;
+      }
+    }
+
+    if(rightColumn != ''){
+      document.getElementById(rightColumn + '_' + rowNumber).appendChild(
+        document.getElementById(cardId)
+      );
+    }
+  }
+
+  moveLeft(cardId) {
+    
+    let currentColId:string = document.getElementById(cardId).parentElement.id;
+    let columnArray:any = currentColId.split("_");
+    let columnName:string = columnArray[0];
+    let rowNumber:string = columnArray[1];
+    console.log("Column Name: " + columnName);
+    console.log("Row Number: " + rowNumber);
+
+    let leftColumn:string = '';
+    switch(columnName) {
+      case "completeColumn": {
+        leftColumn = "reviewColumn";
+        break;
+      }
+      case "reviewColumn": {
+        leftColumn = "inprogressColumn";
+        break;
+      }
+      case "inprogressColumn": {
+        leftColumn = "todoColumn";
+        break;
+      }
+      default: {
+        console.log("Invalid: No more columns to the left!")
+        break;
+      }
+    }
+
+    if(leftColumn != ''){
+      document.getElementById(leftColumn + '_' + rowNumber).appendChild(
+        document.getElementById(cardId)
+      );
+    }
+  }
 }
