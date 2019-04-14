0a8fc409920f5d60b3e2aafb834b3c9b7a7c1cbc
Swiftvalar <44329379+Swiftvalar@users.noreply.github.com>
2019-04-14 00:22:31 -0500

    Generate Card
    Moved code to generate cards into it's own function separate from the newCard function.

--- src/app/tasklist/tasklist.component.ts
+++ src/app/tasklist/tasklist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery/dist/jquery.min.js';
+import { generate } from 'rxjs';
 
 @Component({
   selector: 'app-tasklist',
@@ -16,45 +17,43 @@ export class TasklistComponent implements OnInit {
   ngOnInit() {
   }
 
-  newCard() {
-    let lastRowElement = document.getElementById('row_' + this.currentNumberOfCards);
-    let newNumberOfCards:number = this.currentNumberOfCards + 1;
-
+  generateCard(rowNumber:number, columnName:string, cardHeader:string, cardTextContent:string ) {
+    
     let row = document.createElement('div');
-    row.id = "row_" + newNumberOfCards;
+    row.id = "row_" + rowNumber;
     row.className = "row justify-content-md-center";
 
     let todoColumn = document.createElement('div');
-    todoColumn.id = "todoColumn_" + newNumberOfCards;
+    todoColumn.id = "todoColumn_" + rowNumber;
     todoColumn.className = "col";
 
     let inprogressColumn = document.createElement('div');
-    inprogressColumn.id = "inprogressColumn_" + newNumberOfCards;
+    inprogressColumn.id = "inprogressColumn_" + rowNumber;
     inprogressColumn.className = "col";
 
     let reviewColumn = document.createElement('div');
-    reviewColumn.id = "reviewColumn_" + newNumberOfCards;
+    reviewColumn.id = "reviewColumn_" + rowNumber;
     reviewColumn.className = "col";
 
     let completeColumn = document.createElement('div');
-    completeColumn.id = "completeColumn_" + newNumberOfCards;
+    completeColumn.id = "completeColumn_" + rowNumber;
     completeColumn.className = "col";
 
     let card = document.createElement('div');
-    card.id = "card_" + newNumberOfCards;
+    card.id = "card_" + rowNumber;
     card.className = "card";
     
     let h5 = document.createElement('h5');
     h5.className = "card-header primary-color white-text";
-    h5.textContent = "Card " + newNumberOfCards;
+    h5.textContent = "Card " + rowNumber;
 
     let cardBody = document.createElement('div');
-    cardBody.id = "cardBody_" + newNumberOfCards;
+    cardBody.id = "cardBody_" + rowNumber;
     cardBody.className = "card-body";
 
     let cardText = document.createElement('div');
     cardText.className = "card-text";
-    cardText.textContent = "I'm card " + newNumberOfCards;
+    cardText.textContent = cardTextContent;
 
     let leftButton = document.createElement('button');
     leftButton.textContent = "Move Left";
@@ -65,19 +64,62 @@ export class TasklistComponent implements OnInit {
     let newCardId:string = card.id;
     rightButton.addEventListener("click", (e:Event) => this.moveRight(card.id));
 
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(leftButton);
+    cardBody.appendChild(rightButton);
+    card.appendChild(h5);
+    card.appendChild(cardBody);
+
+    switch(columnName) {
+      case "todoColumn": {
+        todoColumn.appendChild(card);
+        break;
+      }
+      case "inprogressColumn": {
+        inprogressColumn.appendChild(card);
+        break;
+      }
+      case "reviewColumn": {
+        reviewColumn.appendChild(card);
+        break;
+      }
+      case "completeColumn": {
+        completeColumn.appendChild(card);
+      }
+      default: {
+        alert("Invalid: Missing columnName!")
+        break;
+      }
+    }
+
+    row.appendChild(todoColumn);
+    row.appendChild(inprogressColumn);
+    row.appendChild(reviewColumn);
+    row.appendChild(completeColumn);
+
+    return row;
+    // $(row).insertAfter(lastRowElement);
+
+    // document.getElementById(row.id).appendChild(todoColumn);
+    // document.getElementById(todoColumn.id).appendChild(card);
+    // document.getElementById(card.id).appendChild(h5);
+    // document.getElementById(card.id).appendChild(cardBody);
+    // document.getElementById(cardBody.id).appendChild(cardText);
+    // document.getElementById(cardBody.id).appendChild(leftButton);
+    // document.getElementById(cardBody.id).appendChild(rightButton);
+    // document.getElementById(row.id).appendChild(inprogressColumn);
+    // document.getElementById(row.id).appendChild(reviewColumn);
+    // document.getElementById(row.id).appendChild(completeColumn);
+  }
+
+
+  newCard() {
+    let lastRowElement = document.getElementById('row_' + this.currentNumberOfCards);
+    let newNumberOfCards:number = this.currentNumberOfCards + 1;
+
+    let row = this.generateCard(newNumberOfCards, 'todoColumn', "New Card " + newNumberOfCards, "This is New Card "+ newNumberOfCards)
+
     $(row).insertAfter(lastRowElement);
-
-    document.getElementById(row.id).appendChild(todoColumn);
-    document.getElementById(todoColumn.id).appendChild(card);
-    document.getElementById(card.id).appendChild(h5);
-    document.getElementById(card.id).appendChild(cardBody);
-    document.getElementById(cardBody.id).appendChild(cardText);
-    document.getElementById(cardBody.id).appendChild(leftButton);
-    document.getElementById(cardBody.id).appendChild(rightButton);
-    document.getElementById(row.id).appendChild(inprogressColumn);
-    document.getElementById(row.id).appendChild(reviewColumn);
-    document.getElementById(row.id).appendChild(completeColumn);
-
     this.currentNumberOfCards = newNumberOfCards;
   }
 
